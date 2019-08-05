910d2554c712c2e24c1f137cab9ec49e9e70038a
CyChang325 <44718763+CyChang325@users.noreply.github.com>
2019-08-05 01:38:54 -0400

    Add files via upload

--- Density2DScript.js
+++ Density2DScript.js
@@ -1,8 +1,8 @@
 function generate2dDensityChart(csvURL, chartID) {
    // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
-width = 299- margin.left - margin.right,
-height = 230 - margin.top - margin.bottom;
+width = 260- margin.left - margin.right,
+height = 220 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select(chartID)
